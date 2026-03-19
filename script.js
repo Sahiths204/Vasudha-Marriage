01d34c3ed3e0c6ad8026ccaac844200563ccb8f1
Sahiths204 <sahiths204@gmail.com>
2026-03-19 22:21:35 +0530

    Integrate slideshow and background music in celebration
    Added slideshow and music functionality to the celebration.

--- script.js
+++ script.js
@@ -9,12 +9,12 @@ let particles = [];
 function createFirework(x, y) {
   for (let i = 0; i < 80; i++) {
     particles.push({
-      x: x,
-      y: y,
+      x,
+      y,
       speedX: (Math.random() - 0.5) * 6,
       speedY: (Math.random() - 0.5) * 6,
       size: Math.random() * 3,
-      color: ⁠ hsl(${Math.random() * 360},100%,50%) ⁠,
+      color: ⁠ hsl(${Math.random()*360},100%,50%) ⁠,
       life: 100
     });
   }
@@ -22,33 +22,60 @@ function createFirework(x, y) {
 
 function animate() {
   ctx.fillStyle = "rgba(0,0,0,0.2)";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillRect(0,0,canvas.width,canvas.height);
 
-  particles.forEach((p, i) => {
+  particles.forEach((p,i)=>{
     p.x += p.speedX;
     p.y += p.speedY;
     p.life--;
 
     ctx.beginPath();
-    ctx.arc(p.x, p.y, p.size, 0, Math.PI * 2);
+    ctx.arc(p.x,p.y,p.size,0,Math.PI*2);
     ctx.fillStyle = p.color;
     ctx.fill();
 
-    if (p.life <= 0) {
-      particles.splice(i, 1);
-    }
+    if(p.life<=0) particles.splice(i,1);
   });
 
   requestAnimationFrame(animate);
 }
+animate();
+
+/* SLIDESHOW + MUSIC */
+let images = [
+  "img1.jpeg",
+  "img2.jpeg",
+  "img3.jpeg",
+  "img4.jpeg",
+  "img5.jpeg"
+];
+
+let index = 0;
+let interval;
 
 function startCelebration() {
-  setInterval(() => {
+  document.getElementById("slideshow").classList.remove("hidden");
+
+  // 🎵 PLAY MUSIC
+  document.getElementById("bgMusic").play();
+
+  // 🎬 SLIDESHOW
+  interval = setInterval(()=>{
+    document.getElementById("slideImage").src = images[index];
+    index = (index + 1) % images.length;
+  },2000);
+
+  // 🎆 FIREWORKS
+  setInterval(()=>{
     createFirework(
-      Math.random() * canvas.width,
-      Math.random() * canvas.height / 2
+      Math.random()*canvas.width,
+      Math.random()*canvas.height/2
     );
-  }, 500);
+  },500);
 }
 
-animate();
+function closeSlideshow() {
+  document.getElementById("slideshow").classList.add("hidden");
+  clearInterval(interval);
+  document.getElementById("bgMusic").pause();
+}
